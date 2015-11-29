02f0293a618e0f51b0ebcabdc04bd254dc901309
fanhl <Dyguest0315>
2015-11-29 16:21:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+#PhotoViewEx
+
+A PhotoView include TOP_CROP and BOTTOM_CROP.
+
+<a href="./graphics/device-2015-11-29-161842.png"><img src="./graphics/device-2015-11-29-161842.png" width="40%"/></a>
+<a href="./graphics/device-2015-11-29-161901.png"><img src="./graphics/device-2015-11-29-161901.png" width="40%"/></a>
+
+#Useage
+
+##Step 1.
+
+    compile 'com.github.chrisbanes.photoview:library:1.2.4'
+
+##Step 2.
+
+copy PhotoViewAttacherEx.java into your Project.
+
+##Step 3.
+
+    private PhotoViewAttacherEx mAttacher;
+
+    ...
+
+    mAttacher = new PhotoViewAttacherEx(mImageView);
+
+    ...
+
+    mAttacher.setScaleType(PhotoViewAttacherEx.ScaleTypeEx.TOP_CROP);
+    mAttacher.setScaleType(PhotoViewAttacherEx.ScaleTypeEx.BOTTOM_CROP);
+
+#License
+
+    This project is licensed under the MIT License.have fun.
---
+++ sample/src/main/java/com/fanhl/photoviewex/MainActivity.java
@@ -0,0 +1,74 @@
+package com.fanhl.photoviewex;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.fanhl.photoview.PhotoViewAttacherEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private Button    topCrop;
+    private Button    bottomCrop;
+    private Button    fixStart;
+    private Button    fixEnd;
+    private Button    fixXY;
+    private Button    fitCenter;
+    private Button    center;
+    private Button    centerCrop;
+    private Button    centerInside;
+
+    PhotoViewAttacherEx mAttacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.mImageView = (ImageView) findViewById(R.id.imageView);
+        this.topCrop = (Button) findViewById(R.id.topCrop);
+        this.bottomCrop = (Button) findViewById(R.id.bottomCrop);
+        this.fixStart = (Button) findViewById(R.id.fixStart);
+        this.fixEnd = (Button) findViewById(R.id.fixEnd);
+        this.fixXY = (Button) findViewById(R.id.fixXY);
+        this.fitCenter = (Button) findViewById(R.id.fitCenter);
+        this.center = (Button) findViewById(R.id.center);
+        this.centerInside = (Button) findViewById(R.id.centerInside);
+        this.centerCrop = (Button) findViewById(R.id.centerCrop);
+
+        mAttacher = new PhotoViewAttacherEx(mImageView);
+
+        // except PhotoViewAttacherEx.ScaleTypeEx.TOP_CROP/BOTTOM_CROP,
+        // you can use PhotoViewAttacherEx.ScaleTypeEx.XXX or ImageView.ScaleType.XXX
+
+        setButtonAction(topCrop, PhotoViewAttacherEx.ScaleTypeEx.TOP_CROP);
+        setButtonAction(bottomCrop, PhotoViewAttacherEx.ScaleTypeEx.BOTTOM_CROP);
+        setButtonAction(fixStart, ImageView.ScaleType.FIT_START);
+        setButtonAction(fixEnd, ImageView.ScaleType.FIT_END);
+        setButtonAction(fixXY, ImageView.ScaleType.FIT_XY);
+        setButtonAction(fitCenter, ImageView.ScaleType.FIT_CENTER);
+        setButtonAction(center, ImageView.ScaleType.CENTER);
+        setButtonAction(centerInside, ImageView.ScaleType.CENTER_INSIDE);
+        setButtonAction(centerCrop, ImageView.ScaleType.CENTER_CROP);
+    }
+
+    private void setButtonAction(Button topCrop, final PhotoViewAttacherEx.ScaleTypeEx type) {
+        topCrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAttacher.setScaleType(type);
+            }
+        });
+    }
+
+    private void setButtonAction(Button topCrop, final ImageView.ScaleType type) {
+        topCrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAttacher.setScaleType(type);
+            }
+        });
+    }
+}
